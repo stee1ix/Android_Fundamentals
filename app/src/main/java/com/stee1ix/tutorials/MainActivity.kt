88fd709e570b56ff611c043d1ce876f2f3dee40d
Rituraj Ranjan <rituraj.ranjan.42@gmail.com>
2022-01-02 16:54:00 +0530

    Initial commit

---
+++ app/src/main/java/com/stee1ix/tutorials/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.stee1ix.tutorials
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+}
